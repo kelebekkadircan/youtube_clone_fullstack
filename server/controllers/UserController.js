53f02b43642533b2e6ad8ac041e1f59ecaeb2708
kelebekkadircan <kelebekkadircan@gmail.com>
2023-11-06 18:23:14 +0300

    day2_Videos

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -7,7 +7,7 @@ import User from '../models/UserModel.js';
 
 export const updateUser = async (req, res, next) => {
 
-    if (req.params.id == req.user.id) {
+    if (req.params.id === req.user.id) {
         try {
             const updatedUser = await User.findByIdAndUpdate(
                 req.params.id,
@@ -34,33 +34,97 @@ export const updateUser = async (req, res, next) => {
 }
 
 
-export const deleteUser = (req, res, next) => {
-    res.json("it is succesful user route")
+export const deleteUser = async (req, res, next) => {
+
+
+    if (req.params.id === req.user.id) {
+        try {
+            await User.findByIdAndDelete(
+                req.params.id
+            )
+
+            res.status(200).json("User has been deleted")
+
+        } catch (err) {
+            next(err)
+        }
+    }
+    else {
+        return next(createError(403, "You can delete only your account!"))
+    }
+
 
 }
 
 
-export const getUser = (req, res, next) => {
-    res.json("it is succesful user route")
+export const getUser = async (req, res, next) => {
+
+
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
+
+    } catch (err) {
+        next(err)
+    }
+
+
 }
 
 
-export const subscribe = (req, res, next) => {
-    res.json("it is succesful user route")
+export const subscribe = async (req, res, next) => {
+
+    try {
+        await User.findByIdAndUpdate(req.user.id,
+            {
+                $push: { subscribedUsers: req.params.id }
+            })
+        await User.findByIdAndUpdate(req.params.id,
+            {
+                $inc: { subscribers: 1 }
+            })
+        res.status(200).json("Subscription Successfull.")
+
+    } catch (err) {
+        next(err)
+    }
 }
 
 
 
-export const unsubscribe = (req, res, next) => {
-    res.json("it is succesful user route")
+export const unsubscribe = async (req, res, next) => {
+    try {
+        await User.findByIdAndUpdate(req.user.id,
+            {
+                $pull: { subscribedUsers: req.params.id }
+            })
+        await User.findByIdAndUpdate(req.params.id,
+            {
+                $inc: { subscribers: -1 }
+            })
+        res.status(200).json("Unsubscription Successfull.")
+
+    } catch (err) {
+        next(err)
+    }
 }
 
 
-export const like = (req, res, next) => {
-    res.json("it is succesful user route")
+export const like = async (req, res, next) => {
+    try {
+
+
+    } catch (err) {
+        next(err)
+    }
 }
 
 
 export const dislike = (req, res, next) => {
-    res.json("it is succesful user route")
+    try {
+
+
+    } catch (err) {
+        next(err)
+    }
 }
